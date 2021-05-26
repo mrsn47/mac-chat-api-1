34e0255fafc3c8a8545b7696903ead76d21b3a6c
mrsn47 <81860116+mrsn47@users.noreply.github.com>
2021-05-26 14:18:20 +0200

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb+srv://admin:admin@cluster0.xnaig.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   "bodyLimit": "100kb"
 }
